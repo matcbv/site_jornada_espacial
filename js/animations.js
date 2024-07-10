6f8fa369fa00e12a52171b954d310f314dc2f2e1
matcbv <matheuscbv23@gmail.com>
2024-07-09 23:11:02 -0300

    Adicao de javascript para as animacoes da tela inicial

--- js/animations.js
+++ js/animations.js
@@ -1,10 +1,48 @@
+// ---------- ROCKET ANIMATION ----------
+
 const milkywayBtn = document.querySelector('#milkyway-btn')
 const andromedaBtn = document.querySelector('#andromeda-btn')
+const milkywayRocket = document.querySelector('#milkyway-rocket')
+const andromedaRocket = document.querySelector('#andromeda-rocket')
 
 milkywayBtn.addEventListener('mouseover', () => {
-    milkywayBtn.classList.add('rocket')
+    milkywayRocket.innerHTML = '🚀'
+    milkywayRocket.classList.add('rocket')
 })
 
 milkywayBtn.addEventListener('mouseout', () => {
-    milkywayBtn.classList.remove('rocket')
+    milkywayRocket.innerHTML = ''
+    milkywayRocket.classList.remove('rocket')
+})
+
+andromedaBtn.addEventListener('mouseover', () => {
+    andromedaRocket.innerHTML = '🚀'
+    andromedaRocket.classList.add('rocket')
+})
+
+andromedaBtn.addEventListener('mouseout', () => {
+    andromedaRocket.innerHTML = ''
+    andromedaRocket.classList.remove('rocket')
+})
+
+// ---------- BEGINNING BUTTON ----------
+
+const beginningBtn = document.querySelector('.beginning-btn')
+
+beginningBtn.addEventListener('click', () => {
+    window.scrollTo({top: 0, behavior: "smooth"})
+})
+
+// ---------- INTRO BUTTON ----------
+
+const startingBtn = document.querySelector('.intro-btn')
+const milkywaySection = document.querySelector('.milkyway-section')
+const introSection = document.querySelector('.intro-section')
+
+startingBtn.addEventListener('click', () => {
+    introSection.classList.add('intro-animation')
+    setTimeout(1, () => {
+        introSection.classList.remove('intro-animation')
+        milkywaySection.scrollIntoView({behavior: "smooth"})
+    })
 })
