71eb6e5d3a14124298f8fa1a2e8194f641db948b
matcbv <matheuscbv23@gmail.com>
2024-07-13 00:55:10 -0300

    Adicao de estilos e imagens para a secao da via lactea e demais correcoes

---
+++ js/milkyay.js
@@ -0,0 +1,4 @@
+const sun = document.querySelector('#sun')
+const earth = document.querySelector('#earth')
+const mars = document.querySelector('#mars')
+const saturn = document.querySelector('#saturn')
