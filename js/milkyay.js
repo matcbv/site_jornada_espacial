650846423308ee5c7a803b1fb37f68e5721adefe
matcbv <matheuscbv23@gmail.com>
2024-07-13 00:59:02 -0300

    Adicao de js

--- js/milkyay.js
+++ js/milkyay.js
@@ -1,3 +1,9 @@
+const beginningBtn = document.querySelector('.beginning-btn')
+
+beginningBtn.addEventListener('click', () => {
+    window.scrollTo({top: 0, behavior: "smooth"})
+})
+
 const sun = document.querySelector('#sun')
 const earth = document.querySelector('#earth')
 const mars = document.querySelector('#mars')
