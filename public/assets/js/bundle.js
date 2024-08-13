ca129a2778b0eec1f02c271d7307196201bfe5b2
matcbv <matheuscbv23@gmail.com>
2024-08-13 01:19:36 -0300

    Ajuste no fetch dos popups

--- public/assets/js/bundle.js
+++ public/assets/js/bundle.js
@@ -736,7 +736,7 @@ if (galaxiesMain) {
         for (_iterator.s(); !(_step = _iterator.n()).done;) {
           var image = _step.value;
           if (element.id === image.id) {
-            fetch("http://localhost:8080/popup/".concat(element.id)).then(function (res) {
+            fetch("https://localhost:8080/popup/".concat(element.id)).then(function (res) {
               if (!res.ok) {
                 throw new Error('Erro na requisição do popup.');
               } else {
