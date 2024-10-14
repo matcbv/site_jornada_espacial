67a67d7ba23d1a5f15d5136e1c33e33a89f62344
Matheus Cerqueira <matheuscbv23@gmail.com>
2024-10-14 17:56:59 -0300

    Adicao de comentarios para documentacao

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,20 @@
-const tailwindcss = require('tailwindcss')
-const autoprefixer = require('autoprefixer')
+/*
+  Como o postcss.config.js está na raíz do projeto, não sera necesário especificar o caminho no webpack.config.js, caso contrário, teríamos que seguir o exemplo abaixo:
 
+    use: [
+    MiniCssExtractPlugin.loader,
+    'css-loader',
+    {
+      loader: 'postcss-loader',
+      options: {
+        postcssOptions: require('./postcss.config.js'), // Usando a configuração externa
+      },
+    },
+  ],
+*/ 
 module.exports = {
-    plugins: [
-      tailwindcss,
-      autoprefixer,
-    ],
-  };
-  
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  }
+}
