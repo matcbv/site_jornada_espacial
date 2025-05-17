0904386e6a2ed916869c19b6e827f42e25bf3af3
matcbv <matheuscbv23@gmail.com>
2025-05-17 18:36:59 -0300

    Correcao do caminho absoluto gerado pelo webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,45 +6,45 @@ const { glob } = require('glob');
 const pathList = glob.sync(path.resolve(__dirname, 'frontend', 'js', '*.js'));
 
 module.exports = {
-    mode: 'production',
-    entry: pathList,
-    output: {
-        path: path.resolve(__dirname, 'public', 'assets'),
-        filename: 'js/bundle.js',
-    },
-    module: {
-        rules: [
-            {
-                exclude: /node_modules/,
-                test: /\.js$/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ["@babel/preset-env"]
-                    },
-                },
-            },
-            {
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg)$/i,
-                type: 'asset/resource',
-                generator: {
-                  filename: 'images/[name][ext]',
-                },
-            },
-        ],
-    },
-    optimization:{
-        minimize: true,
-        minimizer: [new CssMinimizerWebpackPlugin()],
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'css/styles.css',
-        }),
-    ],
-    devtool: 'source-map',
+	mode: 'production',
+	entry: pathList,
+	output: {
+		path: path.join(process.cwd(), 'public', 'assets'),
+		filename: 'js/bundle.js',
+	},
+	module: {
+		rules: [
+			{
+				exclude: /node_modules/,
+				test: /\.js$/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env'],
+					},
+				},
+			},
+			{
+				test: /\.css$/,
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+			},
+			{
+				test: /\.(png|jpe?g|gif|svg)$/i,
+				type: 'asset/resource',
+				generator: {
+					filename: 'images/[name][ext]',
+				},
+			},
+		],
+	},
+	optimization: {
+		minimize: true,
+		minimizer: [new CssMinimizerWebpackPlugin()],
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: 'css/styles.css',
+		}),
+	],
+	devtool: 'source-map',
 };
